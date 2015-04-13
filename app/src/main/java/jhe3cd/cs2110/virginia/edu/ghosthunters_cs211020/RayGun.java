d52fcd27a909deb559e2cc75044e0032471972f9
d-wire <ljd3za@virginia.edu>
2015-04-13 18:49:15 -0400

    Updated RayGun and Ray classes.

--- app/src/main/java/jhe3cd/cs2110/virginia/edu/ghosthunters_cs211020/RayGun.java
+++ app/src/main/java/jhe3cd/cs2110/virginia/edu/ghosthunters_cs211020/RayGun.java
@@ -1,6 +1,9 @@
 package jhe3cd.cs2110.virginia.edu.ghosthunters_cs211020;
 
+import android.app.usage.UsageEvents;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.view.MotionEvent;
 
 /**
  * Created by liamj_000 on 4/6/2015.
@@ -16,9 +19,15 @@ public class RayGun extends Item {
         this.ray = ray;
     }
 
-    public void update() {
+    public void updateTouch(float xPos, float yPos) {
+        Point target = new Point((int) xPos, (int) yPos);
+//        Ray ray1 = new Ray(R.drawable.ray, this.getCentralPoint().x, this.getCentralPoint().y, this.xMax, this.yMax,
+//               10, 10, main, target);
+//        ray1.update();
         // If onTouchEvent then create a new ray, set it's target to the coordinates of the touch, and update it.
-    }
+        }
+
+
 
     public void activate() {
 
