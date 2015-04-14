606d7bfe7d0daeee5f1788082f61624281b0dfd1
mpt5nm <mpt5nm@virginia.edu>
2015-04-13 20:23:44 -0400

    hello

--- app/src/main/java/jhe3cd/cs2110/virginia/edu/ghosthunters_cs211020/Item.java
+++ app/src/main/java/jhe3cd/cs2110/virginia/edu/ghosthunters_cs211020/Item.java
@@ -1,5 +1,5 @@
 package jhe3cd.cs2110.virginia.edu.ghosthunters_cs211020;
-
+import java.util.*;
 /**
  * Created by Jimmy on 3/30/2015.
  */
@@ -8,6 +8,9 @@ public class Item extends Entity
     double duration;
     protected String itemID;
     protected boolean activated;
+
+    protected TreeMap <String,Integer> itemFrequency = new TreeMap <String,Integer>();
+    protected ArrayList <String> randomList = new ArrayList<String>();
 
     public void activate() {
         activated = true;
@@ -24,6 +27,16 @@ public class Item extends Entity
         this.itemID = itemID;
         this.duration = duration;
         activated = false;
+
+        itemFrequency.put("shield",30);
+        itemFrequency.put("timeFreezer",15);
+        itemFrequency.put("extraHealth",30);
+
+        for(String id : itemFrequency.keySet())
+        {
+            for(int i=0; i < (int)itemFrequency.get(id); i++)
+                randomList.add(id);
+        }
     }
 
     public void randomlyGenerate()
@@ -40,6 +53,7 @@ public class Item extends Entity
 
     public void destroyer(String destroyer)
     {
+
         if (destroyer.equals(main.getBall().DESTROYER_ID)){
             main.getBall().setItemStored(this);
         }
@@ -51,6 +65,13 @@ public class Item extends Entity
     {
         return itemID;
     }
+
+    public String getRandomItemID()
+    {
+       int index = (int)Math.random()*randomList.size();
+       return randomList.get(index);
+    }
+
 
 
 
